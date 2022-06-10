d5eb2d7668eff6393157e94e3f970f2b7f40b35a
Marius Butz <mbpictures13@gmail.com>
2022-06-10 20:21:01 +0200

    set up communication with microservices

--- legacy/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
+++ legacy/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
@@ -3,21 +3,41 @@ package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
 import hska.iwi.eShopMaster.model.businessLogic.manager.Category;
 import hska.iwi.eShopMaster.model.businessLogic.manager.CategoryManager;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public class CategoryManagerImpl implements CategoryManager{
-	
+public class CategoryManagerImpl implements CategoryManager {
+
+	private final String BASE_URI = System.getenv("CATEGORY_URL");
+	private final WebClient webClient = WebClient.create(BASE_URI);
+
 	public CategoryManagerImpl() {
 	}
 
 	public List<Category> getCategories() {
-		return new ArrayList<Category>();
+		return Arrays.asList(Objects.requireNonNull(webClient
+				.get()
+				.uri("/category")
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Category[].class)
+				.block()));
 	}
 
 	public Category getCategory(int id) {
-		return null;
+		return Objects.requireNonNull(webClient
+				.get()
+				.uri(uriBuilder -> uriBuilder
+						.path("/category/{id}")
+						.build(id))
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Category.class)
+				.block());
 	}
 
 	public Category getCategoryByName(String name) {
@@ -25,14 +45,27 @@ public class CategoryManagerImpl implements CategoryManager{
 	}
 
 	public void addCategory(String name) {
+		Category product = new Category(name);
+		Objects.requireNonNull(webClient
+				.post()
+				.uri("/category")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(product)
+				.retrieve()
+				.bodyToMono(Integer.class)
+				.block());
 	}
 
 	public void delCategory(Category cat) {
-
-// 		Products are also deleted because of relation in Category.java
+		delCategoryById(cat.getId());
 	}
 
 	public void delCategoryById(int id) {
-
+		webClient
+				.delete()
+				.uri(uriBuilder -> uriBuilder
+						.path("/category/{id}")
+						.build(id))
+				.retrieve();
 	}
 }
--- legacy/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ProductManagerImpl.java
+++ legacy/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ProductManagerImpl.java
@@ -2,26 +2,56 @@ package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
 import hska.iwi.eShopMaster.model.businessLogic.manager.Product;
 import hska.iwi.eShopMaster.model.businessLogic.manager.ProductManager;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ProductManagerImpl implements ProductManager {
-	
+	private final String BASE_URI = System.getenv("PRODUCT_URL");
+	private final WebClient webClient = WebClient.create(BASE_URI);
+
 	public ProductManagerImpl() {
 	}
 
 	public List<Product> getProducts() {
-		return new ArrayList<Product>();
+		return Arrays.asList(Objects.requireNonNull(webClient
+				.get()
+				.uri("/products")
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Product[].class)
+				.block()));
 	}
 
 	public List<Product> getProductsForSearchValues(String searchDescription,
 													Double searchMinPrice, Double searchMaxPrice) {
-		return new ArrayList<Product>();
+		return Arrays.asList(Objects.requireNonNull(webClient
+				.get()
+				.uri(uriBuilder -> uriBuilder
+						.path("/products")
+						.queryParam("query", searchDescription)
+						.queryParam("minPrice", searchMinPrice)
+						.queryParam("maxPrice", searchMaxPrice)
+						.build())
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Product[].class)
+				.block()));
 	}
 
 	public Product getProductById(int id) {
-		return null;
+		return Objects.requireNonNull(webClient
+				.get()
+				.uri(uriBuilder -> uriBuilder
+						.path("/products/{id}")
+						.build(id))
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Product.class)
+				.block());
 	}
 
 	public Product getProductByName(String name) {
@@ -29,11 +59,25 @@ public class ProductManagerImpl implements ProductManager {
 	}
 	
 	public int addProduct(String name, double price, int categoryId, String details) {
-		return 0;
+		Product product = new Product(name, price, categoryId, details);
+		return Objects.requireNonNull(webClient
+				.post()
+				.uri("/products")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(product)
+				.retrieve()
+				.bodyToMono(Integer.class)
+				.block());
 	}
 	
 
 	public void deleteProductById(int id) {
+		webClient
+				.delete()
+				.uri(uriBuilder -> uriBuilder
+						.path("/products/{id}")
+						.build(id))
+				.retrieve();
 	}
 
 	public boolean deleteProductsByCategoryId(int categoryId) {
